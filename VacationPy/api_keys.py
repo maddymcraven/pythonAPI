dbaad0fcd280daf36c4dc0233f6047a1486430d5
maddymcraven <maddymcraven@gmail.com>
2020-06-21 13:12:08 -0500

    weatherpy

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "6e82b14dc2f156a45b51c9f5d0bc8bbb"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyC-BWpNNzLyhbQzjJ2Z-O4KLvjOoTzuaug"
